986782a0cfbe58e258e64125155a7500621f8ac8
Viktor Ovcharov <ovcharov.2v@gmail.com>
2020-10-02 22:32:21 +0400

    feat: complete Typical Arrays Problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array && array.length > 0)
+    return array.reduce((min, current) => (current < min) ? current : min, Number.MAX_SAFE_INTEGER)
+  else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array && array.length > 0)
+    return array.reduce((max, current) => (current > max) ? current : max, Number.MIN_SAFE_INTEGER)
+  else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array && array.length > 0)
+    return array.reduce((sum, current) => sum + current, 0) / array.length
+  else return 0;
 }
